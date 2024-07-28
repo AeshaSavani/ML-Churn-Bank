80b32b4339173d6ede12f76e7fe3db4f11687b9e
AeshaSavani <aeshasavani1612@gmail.com>
2024-07-27 22:08:35 -0400

    Removed unneccessary files and added pickle files along with pynb files

--- app/model.py
+++
@@ -1,8 +0,0 @@
-import joblib
-
-# Load the model
-model = joblib.load('model.pkl')
-
-def predict(data):
-    prediction = model.predict(data)
-    return prediction
--- run.sh
+++
@@ -1,3 +0,0 @@
-# #!/bin/bash
-# gunicorn -w 4 -b 0.0.0.0:5000 app.main:app
-# chmod +x run.sh
